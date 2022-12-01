8db637e8e677da351bd8ef38b71cde97736c37a1
Michael Rauter <michael.rauter@picard.de>
2022-12-01 16:29:26 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val list  = mutableListOf<Int>()
+
+            input.forEach {
+            if (it.isNotEmpty()) {
+                sum += it.toInt()
+            } else{
+                list.add(sum)
+                sum =0
+            }
+        }
+        return list.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val list  = mutableListOf<Int>()
+
+        input.forEach {
+            if (it.isNotEmpty()) {
+                sum += it.toInt()
+            } else {
+                list.add(sum)
+                sum =0
+            }
+        }
+        if (input.last().isNotEmpty()){
+            list.add(input.last().toInt())
+        }
+        val sortedList = list.sortedByDescending { it }
+        return sortedList[0] + sortedList[1] + sortedList[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
